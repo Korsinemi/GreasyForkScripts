e059e1aa2f38792bce38099be02206384f109da4
Kori <furrycality@gmail.com>
2024-03-21 21:14:59 -0500

    Create oneko.korsinemi.js

---
+++ scripts/oneko.korsinemi.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         🐱 Oneko | El gatito que sigue tu cursor
+// @namespace    https://github.com/Korsinemi
+// @version      2.5
+// @description  A simple script for just put a kitty following your cursor.
+// @icon         https://en.help.roblox.com/hc/article_attachments/7997146649876/Roblox_Verified_Badge.png
+// @supportURL   https://gist.github.com/Korsinemi/fbd0aba2579c62553f14db72a0a1b228
+// @author       Korsinemi
+// @match        *://*/*
+// @grant        GM_xmlhttpRequest
+// @license      MIT
+ 
+// @name:zh-CN   🐱 Oneko | 跟随你的光标的小猫
+// @description:zh-CN  一个简单的脚本，只是把一个小猫放在你的光标后面。
+// @name:es      🐱 Oneko | El gatito que sigue tu cursor
+// @description:es Un simple script que pone un gatito siguiendo tu cursor.
+// @name:hi      🐱 Oneko | आपके कर्सर का पीछा करने वाली बिल्ली
+// @description:hi एक सरल स्क्रिप्ट जो बस आपके कर्सर का पीछा करने वाली एक बिल्ली को डालती है।
+// @name:ar      🐱 Oneko | القطة التي تتبع المؤشر الخاص بك
+// @description:ar سكريبت بسيط يضع قطة تتبع المؤشر الخاص بك.
+// @name:pt      🐱 Oneko | O gatinho que segue o seu cursor
+// @description:pt Um simples script que coloca um gatinho seguindo o seu cursor.
+// @name:ru      🐱 Oneko | Котенок, который следует за вашим курсором
+// @description:ru Простой скрипт, который просто ставит котенка, следующего за вашим курсором.
+// @name:ja      🐱 Oneko | カーソルを追う子猫
+// @description:ja カーソルを追う子猫を置くだけのシンプルなスクリプト。
+// @name:de      🐱 Oneko | Das Kätzchen, das deinem Cursor folgt
+// @description:de Ein einfaches Skript, das einfach eine Katze setzt, die deinem Cursor folgt.
+// @name:fr      🐱 Oneko | Le chaton qui suit votre curseur
+// @description:fr Un simple script qui met juste un chaton qui suit votre curseur.
+// ==/UserScript==
+
+GM_xmlhttpRequest({
+  method: "GET",
+  url: "https://raw.githubusercontent.com/adryd325/oneko.js/main/oneko.js",
+  onload: function(response) {
+    var script = document.createElement('script');
+    script.textContent = response.responseText;
+    document.body.appendChild(script);
+  }
+});
+
+// Oneko.js by Adryd325
+// GreasyFork version by Korsinemi 💜
