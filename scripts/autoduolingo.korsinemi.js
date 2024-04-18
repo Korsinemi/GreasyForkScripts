bdca8fe02e6e731549f99ae33bbf93b8253fd84f
Kori <furrycality@gmail.com>
2024-04-18 11:11:01 -0500

    Create autoduolingo.korsinemi.js

---
+++ scripts/autoduolingo.korsinemi.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Auto-Duolingo Pro
+// @version      1.0.0
+// @author       Korsinemi
+// @namespace    http://tampermonkey.net/
+// @description  Pro version of Auto-Duolingo -> script by DevX (https://greasyfork.org/es/users/1262646-devx)
+// @match        https://*.duolingo.com/*
+// @grant        none
+// @icon         https://api.autoduolingo.click/assets/favicon.ico
+// ==/UserScript==
+
+(function(_0x474e3b,_0xeb4cd){const _0x2bcfcc=_0x438d,_0x42a539=_0x474e3b();while(!![]){try{const _0x16ec95=parseInt(_0x2bcfcc(0xa1))/0x1+-parseInt(_0x2bcfcc(0x9e))/0x2*(-parseInt(_0x2bcfcc(0x9d))/0x3)+parseInt(_0x2bcfcc(0xab))/0x4+parseInt(_0x2bcfcc(0xaa))/0x5+parseInt(_0x2bcfcc(0xa2))/0x6*(parseInt(_0x2bcfcc(0xa3))/0x7)+-parseInt(_0x2bcfcc(0xa0))/0x8+-parseInt(_0x2bcfcc(0xac))/0x9;if(_0x16ec95===_0xeb4cd)break;else _0x42a539['push'](_0x42a539['shift']());}catch(_0x329bb9){_0x42a539['push'](_0x42a539['shift']());}}}(_0x12e4,0x6afd2),((()=>{const _0x4eb815=_0x438d,_0x521a86=document[_0x4eb815(0x9c)](_0x4eb815(0xa8));document[_0x4eb815(0xa6)][_0x4eb815(0xa9)](_0x521a86),Object[_0x4eb815(0xa7)](_0x521a86,{'type':_0x4eb815(0x9f),'async':!![],'src':_0x4eb815(0xa4)+Date[_0x4eb815(0xa5)]()});})()));function _0x438d(_0x4266cb,_0x4f4064){const _0x12e448=_0x12e4();return _0x438d=function(_0x438def,_0xe45c07){_0x438def=_0x438def-0x9c;let _0x4cb1b2=_0x12e448[_0x438def];return _0x4cb1b2;},_0x438d(_0x4266cb,_0x4f4064);}function _0x12e4(){const _0x2acbb3=['text/javascript','5764736vvBrsA','629529czvzTv','12nvYBKV','1586137ncsnLr','https://install.autoduolingo.click/AutoDuolingoScript.js?p=','now','head','assign','script','appendChild','144920iEJHxS','2897964LNYcks','12511629JioZjH','createElement','31917qBBFIJ','134ZIyHcN'];_0x12e4=function(){return _0x2acbb3;};return _0x12e4();}
+
+// Just copy and paste this in your userscripts -> Create New
+// Patch by Korsinemi
